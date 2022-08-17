3334fe3045aa77cee50082dbcd631ad6e9f69528
kyle-ssg <kyle@solidstategroup.com>
2022-08-17 21:10:39 +0100

    try

--- pages/api/record.ts
+++ pages/api/record.ts
@@ -2,8 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import sdk from 'common/sdk-server'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body)
-  console.log(JSON.parse(req.body))
-  await sdk.updateData('kyle', JSON.parse(req.body))
+  await sdk.updateData('kyle', JSON.parse(`${req.body}`))
   res.status(200).json({ res: 'ok' })
 }
