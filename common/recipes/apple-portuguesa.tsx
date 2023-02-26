4dbe76448cb1760c1418796d3efad269cf559af2
kyle-ssg <kyle@solidstategroup.com>
2023-02-26 14:47:56 +0000

    Adjust recipe

--- common/recipes/apple-portuguesa.tsx
+++ common/recipes/apple-portuguesa.tsx
@@ -5,14 +5,14 @@ export default {
   instructions: `
   # Apple Portuguessa
   
-- Add **Ice**
-- Add **Coffee Liquor** and **Elderflower Liquor**
+- Add **Coffee Liquor**, **Apple Juice** and **Elderflower Liquor**
 - Stir
 - Top up with **Tonic**
 `,
   ingredients: [
     VolumeIngredient('Coffee Liquor', 15, 'ml'),
     VolumeIngredient('Elderflower Liquor', 35, 'ml'),
+    VolumeIngredient('Apple Juice', 50, 'ml'),
     MiscIngredient('Tonic', 1),
     MiscIngredient('Cube of Ice', 1),
     MiscIngredient('Peel of Pear (Optional)', 1),
