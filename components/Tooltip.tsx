9edfada44f6130be15ca2ceadeb0328dd3f7b2e5
kyle-ssg <kyle@solidstategroup.com>
2023-02-26 13:50:30 +0000

    click outside

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,15 +1,20 @@
 import React, { FC, useRef, useState } from 'react'
 import { Utils } from '../common/utils' // we need this to make JSX compile
 import { Tooltip as _Tooltip } from 'reactstrap'
+import useOnClickOutside from 'components/base/useClickOutside'
 
 type TooltipType = {}
 
 const Tooltip: FC<TooltipType> = ({ children, id }) => {
   const [isOpen, toggle] = useState(false)
-
+  const ref = useRef()
+  useOnClickOutside(ref, () => {
+    toggle(false)
+  })
   return (
     <>
       <span
+        ref={ref}
         onMouseEnter={() => toggle(!isOpen)}
         onMouseLeave={() => toggle(false)}
         className='ms-2 fa fa-info-circle'
