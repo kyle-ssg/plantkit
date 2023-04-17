1130961324cd52ec4770cfa5510ce06dc3250ce9
kyle-ssg <kyle@solidstategroup.com>
2023-04-17 15:26:09 +0100

    Tortillas

--- common/recipes/tortillas.tsx
+++ common/recipes/tortillas.tsx
@@ -1,21 +1,25 @@
-import { CupIngredient, TeaspoonIngredient } from './Ingredient'
+import { CupIngredient, TbspIngredient, TeaspoonIngredient } from './Ingredient'
 
 export default {
   title: 'Tortillas',
   instructions: `
 # Tortillas
 
-- Add **Masa harina**
+- Add **Masa harina** and **Flour**
 - Add **Warm water**
-- Mix with for around **5 mins** 
+- Mix until the consistency of play dough 
 - Rest for around **20 mins** under a damp towel
-- Pull off into **28 gram** balls 
-- 15-20 seconds on each side * 3
-- Put back under towel 
+- Pull off into **32 gram** balls 
+- 15 seconds each side
+- 50 seconds on each side
+- Put into tortilla pouch 
 `,
   ingredients: [
     TeaspoonIngredient('Salt', 0.25),
-    CupIngredient('Masa harina', 1.5),
-    CupIngredient('Warm Water', 1),
+    CupIngredient('Masa harina', 0.5),
+    CupIngredient('Flour', 0.5),
+    TbspIngredient('Fat(Any)', 1),
+    TeaspoonIngredient('Honey', 0.5),
+    CupIngredient('Warm Water', 0.75 / 2),
   ],
 }
