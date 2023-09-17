2f1c18eeee2e4c0f65f807eaed5b0d63ce2f1dcf
kyle-ssg <kyle@solidstategroup.com>
2023-09-17 14:47:08 +0100

    Coffee tips

--- common/recipes/coffee.tsx
+++ common/recipes/coffee.tsx
@@ -5,10 +5,13 @@ export default {
   instructions: `
   # Coffee
   
-  - **18g** in **50g** out in **30 seconds** - Light
+  - Espresso: see finger print / not gritty
+  
+  - **18g** in **50g** out in **25-30 seconds** - Light
   - **18g** in **36g** out in **25 seconds** - Medium / Dark
   
-  
+  - Make sure porter filter is dry
+  - Darker roasts should be coarser 
   - **Sour = Short/not long enough**  : (Lemon sharp / feel on side of tongue)
   - **Bitter = long/too long** (strong / ashy (Coating in mouth))
 
