05bcb469377b217da2c45075ecf2ad2189214b37
kyle-ssg <kyle@solidstategroup.com>
2023-12-03 12:32:20 +0000

    Add roast chicken

--- common/recipes.ts
+++ common/recipes.ts
@@ -41,6 +41,8 @@ export const recipes = sortBy(
     require('common/recipes/simple-margarita').default,
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('common/recipes/carbonara').default,
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('common/recipes/roast-chicken').default,
   ],
   'title',
 )
---
+++ common/recipes/roast-chicken.tsx
@@ -0,0 +1,27 @@
+import { GramIngredient, MiscIngredient, MlIngredient } from './Ingredient'
+
+export default {
+  title: 'Pizza Dough',
+  instructions: `
+  ## Pizza Dough 
+  **Note: needs 6 hours total**
+- Pull chicken out **1 hour before**
+- Rub chicken with neutral oil, season with rosemary salt
+- Stuff with slice of lemon, shallots and 2 cloves garlic
+- Bake potato - 45 mins 220f
+- Add chicken 
+- After 20 mins add veg
+- Take potato out after 5 mins 
+- Spoon out potato after 15 mins
+- Add splash of hot milk and stir in butter a bit oat a time on low
+- Take out chicken at 160f at thickest part of breast from back
+`,
+  ingredients: [
+    MiscIngredient('Whole Chicken', 1),
+    MiscIngredient('Lemon', 0.5),
+    MiscIngredient('Salt', 1),
+    MiscIngredient('Potatoes', 4),
+    MiscIngredient('Milk', 1),
+    MiscIngredient('Butter', 1),
+  ],
+}
